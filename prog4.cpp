88b7f378e755e62aa877065bff45cfbc54cc0b2e
saloniajmera1999 <50094565+saloniajmera1999@users.noreply.github.com>
2019-11-17 06:02:36 -0800

    Add files via upload

---
+++ prog4.cpp
@@ -0,0 +1,14 @@
+//program to add numbers
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=5; 
+	int b=-6.5;
+	int sum=a+b;/*adding the two  int float integers
+	                to get the sum*/  
+	cout<<"//Sum:  "<<sum<<endl;	
+	return 0; 
+}
+
